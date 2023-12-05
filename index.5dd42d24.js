3ada20695b3ba52c7bab24898d57393764653006
Denys209818 <deniskravcuk63@gmail.com>
2023-12-05 22:10:50 +0200

    make build

---
+++ index.5dd42d24.js
@@ -0,0 +1,3 @@
+document.getElementById("main-form").addEventListener("submit",function(e){e.preventDefault(),e.stopPropagation(),document.querySelector(".main__form-input").value=""});//# sourceMappingURL=index.5dd42d24.js.map
+
+//# sourceMappingURL=index.5dd42d24.js.map
